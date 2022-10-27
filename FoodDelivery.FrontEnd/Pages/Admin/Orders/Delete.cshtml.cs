77c79cda47b63841ab617e68110f098806e82276
Dominik Lichota <dominik.lichota@devoteam.com>
2022-10-27 10:13:06 +0200

    Admin/Orders is done

--- FoodDelivery.FrontEnd/Pages/Admin/Orders/Delete.cshtml.cs
+++ FoodDelivery.FrontEnd/Pages/Admin/Orders/Delete.cshtml.cs
@@ -1,3 +1,5 @@
+using FoodDelivery.FrontEnd.Models;
+using FoodDelivery.FrontEnd.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,36 @@ namespace FoodDelivery.FrontEnd.Pages.Admin.Orders
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly IOrderService _order;
+        public int Id { get; set; }
+
+        public DeleteModel(IOrderService order)
         {
+            this._order = order;
+        }
+
+        public Account? Account { get; set; }
+
+        public IActionResult OnGetAsync(int id)
+        {
+
+            var check = HttpContext.Session.GetObject<Account>("Admin");
+            if (check == null)
+            {
+                return Redirect("/Index");
+            }
+            Id = id;
+            return Page();
+        }
+        public IActionResult OnPostBack()
+        {
+            return Redirect("/Admin/Orders");
+        }
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            await _order.Delete(id);
+            return Redirect("/Admin/Orders");
+
         }
     }
 }
